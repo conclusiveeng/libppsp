8efd6f0e19b27bd0678c2ed578a2d05219410510
Wojciech Kloska <wojciech@conclusive.pl>
2020-12-11 01:32:12 +0100

    UPDATED: move structures from peer_handler.h to internal.h

--- include/peregrine/peer_handler.h
+++ include/peregrine/peer_handler.h
@@ -30,6 +30,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+enum ppspp_handshake_type { HANDSHAKE_INIT = 0, HANDSHAKE_CLOSE, HANDSHAKE_ERROR };
+
 enum ppspp_protocol_options_headers {
 	F_VERSION = 0,
 	F_MINIMUM_VERSION,
@@ -100,104 +102,7 @@ enum pg_handshake_option {
 	HANDSHAKE_OPT_END = 255
 };
 
-enum ppspp_handshake_type { HANDSHAKE_INIT = 0, HANDSHAKE_CLOSE, HANDSHAKE_ERROR };
-
-struct msg_handshake {
-	uint32_t src_channel_id;
-	uint8_t protocol_options[];
-} __attribute__((packed));
-
-struct msg_handshake_opt {
-	uint8_t code;
-	uint8_t value[];
-} __attribute__((packed));
-
-/**
- * @brief Additional handshake structure used for replying to others peer handshake.
- *
- */
-struct msg_handshake_reply {
-	uint32_t dst_channel_id;
-	uint8_t f_handshake_type;
-	uint32_t src_channel_id;
-	uint8_t f_version;
-	uint8_t version;
-	uint8_t f_min_version;
-	uint8_t min_version;
-	uint8_t f_content_prot_method;
-	uint8_t content_prot_method;
-	uint8_t f_merkle_hash_func;
-	uint8_t merkle_hash_func;
-	uint8_t f_chunk_addr_method;
-	uint8_t chunk_addr_method;
-	uint8_t end_opt;
-} __attribute__((packed));
-
-struct msg_have {
-	uint32_t start_chunk;
-	uint32_t end_chunk;
-} __attribute__((packed));
-
-struct msg_data {
-	uint32_t start_chunk;
-	uint32_t end_chunk;
-	uint64_t timestamp;
-	uint8_t data[];
-} __attribute__((packed));
-
-struct msg_ack {
-	uint32_t start_chunk;
-	uint32_t end_chunk;
-	uint64_t sample;
-};
-
-struct msg_integrity {
-	uint32_t start_chunk;
-	uint32_t end_chunk;
-	uint8_t hash[20];
-} __attribute__((packed));
-
-struct msg_signed_integrity {
-	uint32_t start_chunk;
-	uint32_t end_chunk;
-	uint64_t timestamp;
-	uint8_t signature[];
-} __attribute__((packed));
-
-struct msg_request {
-	uint32_t start_chunk;
-	uint32_t end_chunk;
-} __attribute__((packed));
-
-struct msg_cancel {
-	uint32_t start_chunk;
-	uint32_t end_chunk;
-} __attribute__((packed));
-
-struct msg_pex_resv4 {
-	in_addr_t ip_address;
-	uint16_t port;
-} __attribute__((packed));
-
-struct msg {
-	uint8_t message_type;
-	union {
-		struct msg_handshake handshake;
-		struct msg_have have;
-		struct msg_data data;
-		struct msg_ack ack;
-		struct msg_integrity integrity;
-		struct msg_pex_resv4 pex_resv4;
-		struct msg_signed_integrity signed_integrity;
-		struct msg_request request;
-		struct msg_cancel cancel;
-	};
-} __attribute__((packed));
-
-struct msg_frame {
-	uint32_t channel_id;
-	struct msg msg;
-};
+struct msg;
 
 ssize_t pg_handle_message(struct pg_peer *peer, struct msg *msg);
 
--- src/internal.h
+++ src/internal.h
@@ -26,6 +26,7 @@
 #ifndef PEREGRINE_INTERNAL_H
 #define PEREGRINE_INTERNAL_H
 
+#include "peregrine/socket.h"
 #include <stdint.h>
 #include <peregrine/mt.h>
 
@@ -51,5 +52,103 @@ struct node {
 	char sha[20 + 1];
 	enum node_state state;
 };
+
+struct msg_handshake {
+	uint32_t src_channel_id;
+	uint8_t protocol_options[];
+} __attribute__((packed));
+
+struct msg_handshake_opt {
+	uint8_t code;
+	uint8_t value[];
+} __attribute__((packed));
+
+/**
+ * @brief Additional handshake structure used for replying to others peer handshake.
+ *
+ */
+struct msg_handshake_reply {
+	uint32_t dst_channel_id;
+	uint8_t f_handshake_type;
+	uint32_t src_channel_id;
+	uint8_t f_version;
+	uint8_t version;
+	uint8_t f_min_version;
+	uint8_t min_version;
+	uint8_t f_content_prot_method;
+	uint8_t content_prot_method;
+	uint8_t f_merkle_hash_func;
+	uint8_t merkle_hash_func;
+	uint8_t f_chunk_addr_method;
+	uint8_t chunk_addr_method;
+	uint8_t end_opt;
+} __attribute__((packed));
+
+struct msg_have {
+	uint32_t start_chunk;
+	uint32_t end_chunk;
+} __attribute__((packed));
+
+struct msg_data {
+	uint32_t start_chunk;
+	uint32_t end_chunk;
+	uint64_t timestamp;
+	uint8_t data[];
+} __attribute__((packed));
+
+struct msg_ack {
+	uint32_t start_chunk;
+	uint32_t end_chunk;
+	uint64_t sample;
+};
+
+struct msg_integrity {
+	uint32_t start_chunk;
+	uint32_t end_chunk;
+	uint8_t hash[20];
+} __attribute__((packed));
+
+struct msg_signed_integrity {
+	uint32_t start_chunk;
+	uint32_t end_chunk;
+	uint64_t timestamp;
+	uint8_t signature[];
+} __attribute__((packed));
+
+struct msg_request {
+	uint32_t start_chunk;
+	uint32_t end_chunk;
+} __attribute__((packed));
+
+struct msg_cancel {
+	uint32_t start_chunk;
+	uint32_t end_chunk;
+} __attribute__((packed));
+
+struct msg_pex_resv4 {
+	in_addr_t ip_address;
+	uint16_t port;
+} __attribute__((packed));
+
+struct msg {
+	uint8_t message_type;
+	union {
+		struct msg_handshake handshake;
+		struct msg_have have;
+		struct msg_data data;
+		struct msg_ack ack;
+		struct msg_integrity integrity;
+		struct msg_pex_resv4 pex_resv4;
+		struct msg_signed_integrity signed_integrity;
+		struct msg_request request;
+		struct msg_cancel cancel;
+	};
+} __attribute__((packed));
+
+struct msg_frame {
+	uint32_t channel_id;
+	struct msg msg;
+};
+
 
 #endif //PEREGRINE_INTERNAL_H
