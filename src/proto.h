4e47833cf3c1d42232b80853ec68d0b0521a9a74
Jakub Klama <jakub@conclusive.pl>
2020-12-16 11:59:25 +0100

    Add license comments.

--- src/proto.h
+++ src/proto.h
@@ -1,6 +1,27 @@
-//
-// Created by jakub on 11.12.2020.
-//
+/*
+ * Copyright (c) 2020 Conclusive Engineering Sp. z o.o.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 
 #ifndef LIBPEREGRINE_PROTO_H
 #define LIBPEREGRINE_PROTO_H
@@ -163,4 +184,4 @@ void pack_dest_chan(struct pg_buffer *buf, uint32_t dst_channel_id);
 void pack_pex_resv4(struct pg_buffer *buf, in_addr_t ip_address, uint16_t port);
 void pack_pex_req(struct pg_buffer *buf);
 
-#endif //LIBPEREGRINE_PROTO_H
+#endif /* LIBPEREGRINE_PROTO_H */
