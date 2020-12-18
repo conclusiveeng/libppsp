815325e4125707b81e0566bfe6d07da905a6587b
Marcin Jabrzyk <marcin@conclusive.pl>
2020-12-18 13:14:16 +0100

    request all chunks - last chunk was ommited

--- src/swarm.c
+++ src/swarm.c
@@ -78,7 +78,7 @@ pg_peerswarm_request_find_fn(uint64_t start, uint64_t end, bool value __unused, 
 
 	state->collected += count;
 	pg_bitmap_set_range(state->ps->want_bitmap, start, start + count - 1, true);
-	pack_request(state->ps->buffer, start, start + count - 1);
+	pack_request(state->ps->buffer, start, start + count);
 	return (state->collected <= state->budget);
 }
 
